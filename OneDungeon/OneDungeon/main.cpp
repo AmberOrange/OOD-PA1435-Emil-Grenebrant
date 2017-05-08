086c87791a8fb306b56fc0f0629bf7569a85bb16
Heso113 <hampe_olsson91@hotmail.com>
2017-05-08 15:07:14 +0200

    Dew är en elak pojke!

--- OneDungeon/OneDungeon/main.cpp
+++ OneDungeon/OneDungeon/main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+
+	string answer;
+
 	cout << "Hello!" << endl;
-	cout << "Dew är en snäll pojke" << endl;
+	cout << "Dew är en snäll pojke! Eller vad tycker du?(y/n): " << endl;
+
+	cin >> answer;
+	cin.ignore();
+
+	if (answer == "y")
+	{
+		cout << "Du har fel, Dew är en elak pojke!" << endl;
+	}
+	else if (answer == "n")
+	{
+		cout << "Du har rätt, Dew är en elak pojke!" << endl;
+	}
+	else
+	{
+		cout << "Du svarade inte ens i rätt format. Men jag antar att du håller med om att Dew är en elak pojke!" << endl;
+	}
+
 
 	cin.ignore();
 
