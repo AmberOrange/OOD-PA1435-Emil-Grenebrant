7633fa9740dc1c0dfc77a848da701ccc0fff56f1
Craiynel <craiynel@gmail.com>
2017-05-10 10:50:03 +0200

    Added class for inventory

---
+++ OneDungeon/OneDungeon/Inventory.h
@@ -0,0 +1,25 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include "IInventory.h"
+
+class Inventory : public IInventory
+{
+public:
+	Inventory();
+	~Inventory();
+
+private:
+
+};
+
+Inventory::Inventory()
+{
+}
+
+Inventory::~Inventory()
+{
+}
+
+#endif // !INVENTORY_H
+
