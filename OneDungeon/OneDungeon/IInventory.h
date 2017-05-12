d6da06535cf298b29a6ae289ab05eb3d559fd962
Craiynel <craiynel@gmail.com>
2017-05-12 15:47:27 +0200

    inventory update (not fixed)

--- OneDungeon/OneDungeon/IInventory.h
+++ OneDungeon/OneDungeon/IInventory.h
@@ -4,17 +4,19 @@
 
 #include "IItem.h"
 #include <string>
+#include <iostream>
 
 class IInventory
 {
 public:
 	virtual ~IInventory() {};
 
-	virtual void equip(std::string choice) = 0;
-	virtual void getGear(std::string choice) = 0;
+	virtual void equip(std::string choice) = 0;		 
+	virtual IItem* getGear(std::string choice) = 0;	// Returned void, now returns IItem* 
 	virtual void use(std::string choice) = 0;
 	virtual bool isItem(std::string choice) = 0;
 	virtual void inventory() = 0;
+	virtual std::string getPlayerChoice() = 0;		// Added
 };
 
 #endif
--- OneDungeon/OneDungeon/Inventory.h
+++ OneDungeon/OneDungeon/Inventory.h
@@ -2,6 +2,8 @@
 #define INVENTORY_H
 
 #include "IInventory.h"
+#include "IPlayer.h"
+#include <vector>
 
 class Inventory : public IInventory
 {
@@ -9,17 +11,28 @@ public:
 	Inventory();
 	~Inventory();
 
+	void equip(std::string, IPlayer* player);		// equip item
+	IItem* getGear(std::string choice);				// if item is gear, retrieve it
+	void use(std::string choice, IPlayer* player);	// use consumables
+	bool isItem(std::string choice);				// will always return true
+	void inventory(IPlayer* player);				// Open inventory
+
 private:
+	std::vector<IItem*> items;
+
+	std::string getPlayerChoice();
 
 };
 
-Inventory::Inventory()
-{
-}
-
-Inventory::~Inventory()
-{
-}
-
 #endif // !INVENTORY_H
+
+
+/*
+Frågor:
+
+item->getString()
+use - ska spelaren aktivera eller inventory?
+isItem ? - vad är syftet med den?
+
+*/
 
